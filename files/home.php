c88019dfa5764910be68e9e2a0fa396b8aae9c69
RAJJAF Sohayeb <93768126+SohayebRa@users.noreply.github.com>
2021-11-28 13:55:35 +0100

    Update files

--- files/home.php
+++ files/home.php
@@ -6,9 +6,33 @@ require 'header.php';
 
 ?>
 
-<div class="container-voz">
+<div class="container-voz home">
     <div class="row pt-4 mx-auto">
-        <h2 class="ps-4">Bienvenido, 'Admin'!</h2>
+        <h2 class="mb-5">Bienvenido, 'Admin'!</h2>
+        <div class="row mb-3">
+            <div class="col-4">
+                <a href="list-hadith.php" class="form-control py-4 text-light text-center text-decoration-none text-decoration-none"><i class="text-warning me-3 fas fa-list"></i>Lista Hadices</a>
+            </div>
+            <div class="col-4">
+                <a href="all-hadith.php" class="form-control py-4 text-light text-center text-decoration-none"><i class="text-warning me-3 far fa-eye"></i>Ver Hadices</a>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col-4">
+                <a href="add-hadith.php" class="form-control py-4 text-light text-center text-decoration-none"><i class="text-warning me-3 far fa-plus-square"></i>Publicar Hadith</a>
+            </div>
+            <div class="col-4">
+                <a href="edit-hadith.php" class="form-control py-4 text-light text-center text-decoration-none"><i class="text-warning me-3 far fa-edit"></i>Modificar Hadith</a>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col-4">
+                <a href="delete-hadith.php" class="form-control py-4 text-light text-center text-decoration-none"><i class="text-warning me-3 fas fa-trash-alt"></i>Borrar Hadith</a>
+            </div>
+            <div class="col-4">
+                <a href="#" class="form-control py-4 text-light text-center text-decoration-none">. . .</a>
+            </div>
+        </div>
     </div>
 </div>
 
